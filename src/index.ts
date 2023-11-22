22650bce2468ab135079442d3e6a2a44b1b29b58
CristhianMotoche <cristhian.motoche@gmail.com>
2023-11-22 08:01:47 -0500

    feat: Add doctests for all exported functions

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,19 @@ type OneOrMany<T> = "One" | "Many"
 */
 export const one = <T>(x: T): OneOrMany<T> => "One"
 
-export const many = <T>(x: RNEA.ReadonlyNonEmptyArray<T>): OneOrMany<T> => "Many"
+/** Smart constructor for a list of elements
 
+    many([]) // => O.none
+    many([1,2,3]) // => { tag: "Many", value: [1,2,3] }
+
+*/
+export const many = <T>(x: T[]): O.Option<OneOrMany<T>> => O.some("Many")
+
+/** Build OneOrMany from a list
+
+    fromList([]) // => O.none
+    fromList([3]) // => { tag: "One", value: 3 }
+    fromList([3, 4, 5]) // => { tag: "Many", value: [3, 4, 5] }
+
+*/
 export const fromList = <T>(x: T[]): O.Option<OneOrMany<T>> => O.some("One")
