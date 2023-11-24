35ef626d4679d0ca4cdc15fbe20411409b09c4c6
CristhianMotoche <cristhian.motoche@gmail.com>
2023-11-23 19:29:22 -0500

    feat: Add some tips

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,27 @@ import * as NEA from 'fp-ts/NonEmptyArray'
 type OneOrMany<T> = { tag: "One", value: T } | { tag: "Many", value: NEA.NonEmptyArray<T> }
 
 const one = <T>(x: T): OneOrMany<T> => ({ tag: "One", value: x })
-const many = <T>(x: NEA.NonEmptyArray<T>): OneOrMany<T> => 
+
+const many = <T>(x: NEA.NonEmptyArray<T>): OneOrMany<T> =>
   ({ tag: "Many", value: x })
 
 /* Build OneOrMany from a list
     fromList([]) // => O.none
     fromList([3]) // => O.some({ tag: "One", value: 3 })
     fromList([3, 4, 5]) // => O.some({ tag: "Many", value: [3, 4, 5] })
+
+    tips:
+      O.map((x) => x + 1)(O.some(1)) === O.some(2)
+      NEA.fromArray([]) === O.none
+      NEA.fromArray([1,2]) === O.some(NonEmptyArray[1, 2])
 */
 export const fromList = <T>(x: T[]): O.Option<OneOrMany<T>> => {
-  return O.some(one(x[1]))
+
+  if (x.length === 0) {
+    return "none"
+  else if(x.length === 1) {
+    return "some one"
+  } else {
+    return "some many"
+  }
 }
